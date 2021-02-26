59773dcaaa4cee035b9bb36ca826d99993b944c7
Jimbo <marquardtjames@gmail.com>
2021-02-25 19:49:40 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span
+      v-for="(letter, index) in message.split('')"
+      :key="index"
+      :class="$style.large"
+      :style="`color: ${colors[index % 10]}`"
+      >{{ letter }}</span
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { schemeCategory10 } from 'd3-scale-chromatic'
+
+export default Vue.extend({
+  data() {
+    return {
+      message: 'hello from vue/d3',
+      colors: schemeCategory10,
+    }
+  },
+  mounted() {
+    console.log('hello')
+  },
+})
+</script>
+
+<style module>
+.large {
+  font-size: 72px;
+}
+</style>
